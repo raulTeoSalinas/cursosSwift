e0720a4eb92a32e16d3905604cfe9641476b2eb8
raulTeoSalinas <raul.teo.salinas@cic.unam.mx>
2024-04-11 17:27:59 -0600

    Fommated organization

--- ex1.swift
+++
@@ -1,7 +0,0 @@
-let medicineName: String = "Humulina"
-let barcodeNumber: String = "18371238712"
-var medicineCost: Double = 605.30
-var unitsSold: Int = 29229823
-var unitsAvailable: Int = 130400
-var isDiscontinued: Bool = false
-let medicineType: Character = "I" // Intravenous
--- relationalOperators.swift
+++
@@ -1,4 +0,0 @@
-let number1: Int = 10
-let number2: Int = 5
-
-print(number1 != number2)
